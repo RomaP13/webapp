98585e15e86838c0a113f7a542d0e0ff3edeb161
Roman Pomazan <romapomazan1310@gmail.com>
2024-07-27 10:28:20 +0300

    feat: add simple front-end part

--- miniapp/webapp/views.py
+++ miniapp/webapp/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 
 
-# Create your views here.
 def index(request):
     return render(request, "index.html")
+
+
+def profile(request):
+    user = {
+        "username": "test_user",
+        "wealth": 1000,
+        "gold": 10,
+        "silver": 990,
+        "photo": "https://via.placeholder.com/150"
+    }
+    return render(request, "profile.html", {"user": user})
